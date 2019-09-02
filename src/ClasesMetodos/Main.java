1a4f50060624c539dfd2bc799aecd3ece444e7cd
kenyv <kenyv@DESKTOP-9ONC57S>
2019-09-02 02:07:11 -0600

    Laboratorio instanciando un objeto con diferentes metodos matematicos.

---
+++ src/ClasesMetodos/Main.java
@@ -0,0 +1,15 @@
+package ClasesMetodos;
+
+public class Main {
+    public static void main(String args[]){
+        Numeros nums = new Numeros(12,90,23,15);
+        
+        nums.showInf();
+        nums.igualdad();
+        nums.MayorMenor();
+        nums.Maximo();
+        nums.Minino();
+        nums.Raiz2();
+        nums.Vieta();
+    }    
+}
